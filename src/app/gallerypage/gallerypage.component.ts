20cf627f6d59271b51ea2f0f554f680eee7745cb
lionel5116 <lioneljones5116@gmail.com>
2020-04-06 19:33:22 -0500

    Fixed Image Gallery

--- src/app/gallerypage/gallerypage.component.ts
+++ src/app/gallerypage/gallerypage.component.ts
@@ -10,14 +10,28 @@ export class GallerypageComponent implements OnInit {
 
   constructor() { }
 
+
   images:Images[];
   ngOnInit() {
+
+    var newline = 'U+0085';
+    var yennyDescrip = "Colombian Chica Caroline-Yenny  \ Met March 2020 in Club Gustos"
+    var myString = 'A rather long string of English text, an error message ' +
+               'actually that just keeps going and going -- an error ' +
+               'message to make the Energizer bunny blush (right through ' +
+               'those Schwarzenegger shades)! Where was I? Oh yes, ' +
+               'you\'ve got an error and all the extraneous whitespace is ' +
+               'just gravy.  Have a nice day.';
     this.images = [];
-    this.images.push(new Images('Caroline Yenny','Colombian Chica Caroline-Yenny','assets/images/CarolineYenny.JPG'));
-    this.images.push(new Images('Vanessa Costa Rica','Costa Rica Chica Vanessa CostaRica','/assets/images/VanessaCostaRica.jpg'));
-    this.images.push(new Images('Juliette','Colombian Chica Juliette','assets/images/Juliette.JPG'));
-    this.images.push(new Images('Nicole','Colombian Nicole Cartejena','assets/images/NicoleCartejena.JPG'));
-    this.images.push(new Images('Samantha','Colombian Samantha Cartejena','assets/images/Samantha.JPG'));
+    this.images.push(new Images('Caroline Yenny', yennyDescrip,' assets/images/CarolineYenny.JPG'));
+
+    this.images.push(new Images('Vanessa Costa Rica','Venezualen - Met In Costa Rica 2019','/assets/images/VanessaCostaRica.jpg'));
+
+    this.images.push(new Images('Juliette','Met In Cartejena - Julliets friend - OTC','assets/images/Juliette.JPG'));
+
+    this.images.push(new Images('Hillary','Colombian - Cartejena - Danielles Friend','assets/images/NicoleCartejena.JPG'));
+
+    this.images.push(new Images('Samantha','10 out 10 - Cartejena (Dulce Vida)','assets/images/Samantha.JPG'));
 
   }
 
